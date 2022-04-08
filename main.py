d6308b7193fc7b52ab5061c9ebbdf7e7592cdfb6
andresgomes <andre_colossus@yahoo.com.br>
2022-04-07 23:55:48 -0300

    ft: (number)# to roll the dices (number) times

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 client = discord.Client()
 deffaut_difficult = 6
 
+
 def simple_result(input_int):
     result_dice = []
     for i in range(input_int):
@@ -16,75 +17,109 @@ def simple_result(input_int):
     result_dice.sort(reverse=True)
     return result_dice
 
-def right_result(input_int, difficult):
+
+def right_result(qtd_dices, difficult):
     hits = 0
     result_dices = []
-    for i in range(input_int):
+    for i in range(qtd_dices):
         dice = random.randrange(1, 11)
         if dice >= difficult:
             hits += 1
         if dice == 1:
             hits -= 1
         result_dices.append(dice)
+    hits = 0 if hits < 0 else hits
     result_dices.sort(reverse=True)
-    return f'{hits} <- {result_dices}'
+    return f'{hits}  ⟵ {result_dices} \n'
 
-def damage_block_result(input_int):
+
+def damage_block_result(input_int, difficult):
     result_dices = []
     hits = 0
     for i in range(input_int):
         dice = random.randrange(1, 11, 1)
-        if dice >= 6:
+        if dice >= difficult:
             hits += 1
         result_dices.append(dice)
     result_dices.sort(reverse=True)
-    return f'{hits} <- {result_dices}'
+    return f'{hits}  ⟵ {result_dices} \n'
 
-#main web page
-@app.route('/')
-def index():
-    return render_template("index.html")
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-#main discord bot
+
+# main discord bot
 @client.event
 async def on_message(message):
     content = str(message.content).lower()
-    author = message.author.name
-    mention = message.author.mention
-    #If just int roll 10
+    # author = message.author.name
+    # mention = message.author.mention
+    # If just int roll 10
+    result = ''
     if content.isnumeric():
         result = simple_result(int(content))
         await message.reply(str(result))
-        #await message.channel.response(str(result))
+        # await message.channel.response(str(result))
 
-    #If start with 'a', count the hits in normal tests with difficult 6 if start with 'ha', count de damage and block
-    elif content.startswith('a') or content.startswith('ha'):
+    # If start with 'a', count the hits in normal tests with difficult 6 if start with 'h', count de damage and block
+    elif content.startswith('a') or content.startswith('h'):
         qtd_dices = ''.join([i for i in content if i.isdigit()])
-        result = ''
-        if content[1].isdigit():
+        if content[0] == 'a':
             result = right_result(int(qtd_dices), deffaut_difficult)
-        elif content[2].isdigit():
-            result = damage_block_result(int(qtd_dices))
+        elif content[0] == 'h':
+            result = damage_block_result(int(qtd_dices), deffaut_difficult)
         await message.reply(result)
 
-    #If start with hd(int)a, use int to apply a another difficult
-    elif content.startswith('hd'):
+    # If start with sa(int)a, use int to apply a another difficult
+    elif content.startswith('sa'):
         difficult = int(content[2])
-        new_input_client = content[4: ]
-        qtd_dices = int(new_input_client)
-        result = right_result(qtd_dices, difficult)
+        qtd_dices = int(content[4:])
+        if content[3] == 'a':
+            result = right_result(qtd_dices, difficult)
+        if content[3] == 'b':
+            result = damage_block_result(qtd_dices, difficult)
         await message.reply(result)
 
-#main web page
+    # if start with #, roll more tests
+    elif content.startswith('#'):
+        position = 1
+        qtd_rolls = ''
+        while content[position].isdigit():
+            qtd_rolls += content[position]
+            position += 1
+        qtd_rolls = range(int(qtd_rolls))
+        content = content[position:]
+        if content.startswith('a') or content.startswith('h'):
+            qtd_dices = ''.join([i for i in content if i.isdigit()])
+            result = ''
+            if content[0] == 'a':
+                for i in qtd_rolls:
+                    result += right_result(int(qtd_dices), deffaut_difficult)
+            elif content[0] == 'h':
+                for i in qtd_rolls:
+                    result += damage_block_result(int(qtd_dices), deffaut_difficult)
+            await message.reply(result)
+
+        elif content.startswith('sa'):
+            difficult = int(content[2])
+            qtd_dices = int(content[4:])
+            if content[3] == 'a':
+                for i in qtd_rolls:
+                    result += right_result(qtd_dices, difficult)
+            if content[3] == 'b':
+                for i in qtd_rolls:
+                    result += damage_block_result(qtd_dices, difficult)
+            await message.reply(result)
+
+
+# main web page
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run()
     client.run(os.getenv('TOKEN'))
-
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,26 @@
-from ..main import simple_result
+from ..main import simple_result, right_result
 import unittest
 
-def test_roll_zero_dices():
+def test_should_return_list_with_no_elements_when_roll_no_dice():
     result = simple_result(0)
     assert len(result) == 0
 
-def test_roll_two_dices():
+def test_should_return_list_with_two_elements_when_roll_2_dices():
     result = simple_result(2)
     assert len(result) == 2
 
-def test_roll_five_dices():
+def test_should_return_list_with_fifteen_elements_when_roll_fiftenn_dice():
     assert len(simple_result(15)) == 15
 
+def test_first_list_element_should_be_between_one_and_10():
+    result = simple_result(1)
+    r = range(1, 11)
+    assert result[0] in r
+
+def test_last_list_element_should_be_between_one_and_10():
+    result = simple_result(10)
+    r = range(1, 11)
+    assert result[-1] in r
 
 if __name__ == '__main__':
     unittest.main()
